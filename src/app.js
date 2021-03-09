d0fa81bdc60b5273bb810e04a2d2eb442bad8b36
Chi Kei Chan <chikeichan@gmail.com>
2021-03-09 00:28:59 -0800

    add cors options (#2)

--- src/app.js
+++ src/app.js
@@ -2,12 +2,24 @@ const logger = require('./logger.js');
 const APIRoot = require('./api/root.js');
 const {container} = require('./container.js');
 const path = require('path');
+const cors = require('cors')
 
 const viewsDir = path.join(__dirname, 'views');
+
+const corsOptions = {
+  origin: function (origin= '', callback) {
+    callback(null, [
+      'http://localhost:8080',
+      'https://shakedex.com',
+      'https://compassionate-yalow-5608e1.netlify.app',
+    ]);
+  }
+};
 
 async function main() {
   const express = require('express');
   const app = express();
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.set('view engine', 'pug');
   app.set('views', viewsDir);
